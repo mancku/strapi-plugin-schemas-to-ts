927506d1647820f8291b5a52fdffd044c521c2c3
Adrià López <adria.lopez.ortega@gmail.com>
2023-05-23 11:31:51 +0200

    Modified plugin name and updated package.json to contain all needed info

--- server/register.ts
+++ server/register.ts
@@ -2,7 +2,7 @@ import { Strapi } from '@strapi/strapi';
 import { PluginConfig } from './models/pluginConfig';
 import { Converter } from './schemas-to-ts/converter';
 
-export const pluginName: string = 'strapi-plugin-schemas-to-ts';
+export const pluginName: string = 'schemas-to-ts';
 
 export default ({ strapi }: { strapi: Strapi }) => {
   const config: PluginConfig = strapi.config.get(`plugin.${pluginName}`);
