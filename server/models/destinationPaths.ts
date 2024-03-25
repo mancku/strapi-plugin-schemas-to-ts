6a3efdedcf5dec65c18373f555a04ecad410b150
Adrià López <adria.lopez.ortega@gmail.com>
2024-03-25 11:28:04 +0100

    Using StrapiDirectories and added some more asserts

--- server/models/destinationPaths.ts
+++ server/models/destinationPaths.ts
@@ -1,8 +1,8 @@
+import { StrapiDirectories } from '@strapi/strapi';
 import path from 'path';
 import { FileHelpers } from '../schemas-to-ts/fileHelpers';
 import { PluginConfig } from './pluginConfig';
 import { pluginName } from './pluginName';
-import { StrapiPaths } from './strapiPaths';
 
 export class DestinationPaths {
   public commons?: string;
@@ -13,7 +13,7 @@ export class DestinationPaths {
   private readonly componentInterfacesFolderName: string = 'interfaces';
   private readonly commonFolderName: string = 'common';
 
-  constructor(config: PluginConfig, strapiPaths: StrapiPaths) {
+  constructor(config: PluginConfig, strapiPaths: StrapiDirectories) {
     let useDefaultFolders: boolean = true;
     let destinationFolder: string = config.destinationFolder;
     if (destinationFolder) {
@@ -25,43 +25,79 @@ export class DestinationPaths {
     }
 
     if (useDefaultFolders) {
-      this.components = FileHelpers.ensureFolderPathExistRecursive(strapiPaths.components, this.componentInterfacesFolderName);
-      this.commons = FileHelpers.ensureFolderPathExistRecursive(strapiPaths.src, this.commonFolderName, config.commonInterfacesFolderName);
+      this.components = FileHelpers.ensureFolderPathExistRecursive(strapiPaths.app.components, this.componentInterfacesFolderName);
+      this.commons = FileHelpers.ensureFolderPathExistRecursive(strapiPaths.app.src, this.commonFolderName, config.commonInterfacesFolderName);
     }
 
     this.useForApis = !!destinationFolder;
   }
 
-  private getFinalDestinationFolder(destinationFolder: string, strapiPaths: StrapiPaths) {
-    if (destinationFolder.startsWith(strapiPaths.root)) {
-      destinationFolder = this.removeStrapiRootPathFromFullPath(destinationFolder, strapiPaths.root);
+  private getFinalDestinationFolder(destinationFolder: string, strapiPaths: StrapiDirectories) {
+    if (destinationFolder.startsWith(strapiPaths.app.root)) {
+      destinationFolder = this.removeStrapiRootPathFromFullPath(destinationFolder, strapiPaths.app.root);
     }
 
-    destinationFolder = path.join(strapiPaths.root, destinationFolder);
-    this.assertDestinationIsInsideStrap(destinationFolder, strapiPaths);
+    destinationFolder = path.join(strapiPaths.app.root, destinationFolder);
+    this.assertDestinationIsInsideStrapi(destinationFolder, strapiPaths);
 
     destinationFolder = FileHelpers.normalizeWithoutTrailingSeparator(destinationFolder);
-    this.assertDestinationIsNotStrapiRoot(destinationFolder, strapiPaths);
+    this.assertDestinationIsNotStrapiCoreFolder(destinationFolder, strapiPaths);
 
-    const relativeRoute: string = this.removeStrapiRootPathFromFullPath(destinationFolder, strapiPaths.root);
+    const relativeRoute: string = this.removeStrapiRootPathFromFullPath(destinationFolder, strapiPaths.app.root);
     const folders: string[] = relativeRoute.split(path.sep).filter(part => part !== '');
-    destinationFolder = FileHelpers.ensureFolderPathExistRecursive(strapiPaths.root, ...folders);
+    destinationFolder = FileHelpers.ensureFolderPathExistRecursive(strapiPaths.app.root, ...folders);
     return destinationFolder;
   }
 
-  private assertDestinationIsNotStrapiRoot(destinationFolder: string, strapiPaths: StrapiPaths) {
-    if (destinationFolder === strapiPaths.root) {
-      throw new Error(`${pluginName} ⚠️  The given destinationFolder is the same as the Strapi root`);
-    }
-  }
-
-  private assertDestinationIsInsideStrap(destinationFolder: string, strapiPaths: StrapiPaths) {
-    if (!destinationFolder.startsWith(strapiPaths.root)) {
+  private assertDestinationIsInsideStrapi(destinationFolder: string, strapiPaths: StrapiDirectories) {
+    if (!destinationFolder.startsWith(strapiPaths.app.root)) {
       throw new Error(`${pluginName} ⚠️  The destination folder is not inside the Strapi project: '${destinationFolder}'`);
     }
   }
 
   private removeStrapiRootPathFromFullPath(destinationFolder: string, strapiRootPath: string): string {
     return path.relative(strapiRootPath, destinationFolder);
+  }
+
+  private assertDestinationIsNotStrapiCoreFolder(destinationFolder: string, strapiPaths: StrapiDirectories) {
+    if (destinationFolder === strapiPaths.app.root) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is the same as the Strapi root`);
+    }
+
+    if (destinationFolder=== strapiPaths.app.src) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is the same as the Strapi src`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.app.api)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi api`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.app.components)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi components`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.app.extensions)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi extensions`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.app.policies)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi policies`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.app.middlewares)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi middlewares`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.app.config)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi config`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.dist.root)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi dist`);
+    }
+    
+    if (destinationFolder.startsWith(strapiPaths.static.public)) {
+      throw new Error(`${pluginName} ⚠️  The given destinationFolder is inside the Strapi static`);
+    }
   }
 }
